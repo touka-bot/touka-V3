c90527b8372ada168f085c8e9d40bb1f1b96947c
shinixsensei <crezoxplays@gmail.com>
2021-03-30 01:18:07 +0200

    v3.0.1

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
     public static void main(String[] args) throws LoginException, IOException {
         DefaultShardManagerBuilder builder =
-                DefaultShardManagerBuilder.createDefault("ODE5MjM3MTU0OTc3ODczOTMw.YEjsDg.n20cgALF7DBbrp1-0RnA3Kg_8ok");
+                DefaultShardManagerBuilder.createDefault("NzgzNzIwNzI1ODQ4MTI5NTY2.X8e2zQ.nbq7lDRxEK9eNebvwY6yfV6qLGk");
         builder.setCompression(Compression.ZLIB);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setAutoReconnect(true);
